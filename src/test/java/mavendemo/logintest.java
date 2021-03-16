efd51ef1f810919a828fa9551aa5d5224d76447e
root <root@ip-172-31-23-103.ec2.internal>
2021-03-16 10:38:53 +0000

    added login and test login

---
+++ src/test/java/mavendemo/logintest.java
@@ -0,0 +1,20 @@
+package mavendemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class loginTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
